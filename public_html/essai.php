2cef0d1f5d2ab7e97f82fe11a4b43bc813929795
Tagdada <audrey.passerieux@outlook.com>
2016-11-13 00:24:01 +0100

    TP3 done

--- public_html/essai.php
+++ public_html/essai.php
@@ -1,6 +1,7 @@
 <html>
   <head>
     <title>Essai !!</title>
+    <meta charset="utf-8">
   </head>
   <body>
     <h1>Mon premier essai PHP</h1>
@@ -12,6 +13,17 @@
     echo $_SERVER["REMOTE_ADDR"];
     ?>
 
+    <h2>Message personnalisé</h2>
+    <?php
+    $day = date("w");
+    $dayName = array("Dimanche","Lundi","Mardi",
+    "Mercredi","Jeudi","Vendredi","Samedi");
+
+    $currentDay = $dayName[$day];
+    echo '<a href="https://fr.wikipedia.org/wiki/'.$dayName[$day].'" >Lien du jour : '.$currentDay.' </a>';
+// Bof pas compris la question... 
+    echo '<a href="http://mySite.com/essai.php?jour='.$day.'" >Lien 2 du jour : '.$currentDay.' </a>';
+     ?>
 
   </body>
 </html>
